a79866d43781146f57171c78ad9b99d4b112036e
Michael-Pham1 <47127433+Michael-Pham1@users.noreply.github.com>
2023-01-14 15:29:39 -0800

    Update PlayerDeath.cs

--- My project/Assets/Scripts/PlayerDeath.cs
+++ My project/Assets/Scripts/PlayerDeath.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerDeath : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.tag == "EnemyDwayne" || collision.gameObject.tag == "EnemyMyeargh" || collision.gameObject.tag == "Asteroid"){
+        if(collision.gameObject.tag != "Platform"){
             Application.Quit();
         }
     }
