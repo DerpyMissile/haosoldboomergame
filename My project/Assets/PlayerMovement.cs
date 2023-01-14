79dd3748edcef99e9e2d9a55c202a1f750de301b
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2023-01-14 15:18:16 -0800

    Jump
    Jump

---
+++ My project/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector3(0, 7f, 0);
+        }
+    }
+}
