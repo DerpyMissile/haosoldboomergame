048091ce6a20b575a564c290a3fc71b103bf0143
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2023-01-14 16:23:30 -0800

    Animation Tree

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animate;
+    private BoxCollider2D collide;
+    private SpriteRenderer sprite;
+
     private float xDir;
+
+    private enum state { idle, running, jumping, falling }
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animate = GetComponent<Animator>();
+        collide = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,5 +31,26 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump")) {
             rb.velocity = new Vector2(rb.velocity.x, 7f);
         }
+    }
+
+    private void AnimationUpdate() {
+        state currState;
+        if (xDir < 0f) {
+            currState = state.running;
+            sprite.flipX = true;
+        } else if (xDir > 0f) {
+            currState = state.running;
+            sprite.flipX = false;
+        } else {
+            currState = state.idle;
+        }
+
+        if (rb.velocity.y > .1f) {
+            currState = state.jumping;
+        } else if (rb.velocity.y < -.1f) {
+            currState = state.falling;
+        }
+
+        animate.SetInteger("currState", (int)currState);
     }
 }
