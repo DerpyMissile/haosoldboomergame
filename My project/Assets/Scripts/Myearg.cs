288ccf905f22e90fe805f8fd5d1043f0526b1061
DerpyMissile <hmai015@ucr.edu>
2023-01-14 17:43:33 -0800

    fkdlsajfklsdajlkfas;jd

--- My project/Assets/Scripts/Myearg.cs
+++ My project/Assets/Scripts/Myearg.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Myearg : MonoBehaviour
 {
     bool startingFromLeft = false;
+    // private Animator animate;
+    // private SpriteRenderer sprite;
+
+    // private enum state { run }
+
     public bool getStartingPos(){
         return startingFromLeft;
     }
@@ -15,4 +20,19 @@ public class Myearg : MonoBehaviour
             startingFromLeft = false;
         }
     }
+
+    // private void Start(){
+    //     animate = GetComponent<Animator>();
+    //     sprite = GetComponent<SpriteRenderer>();
+    // }
+
+    // private void Update(){
+    //     AnimationUpdate();
+    // }
+
+    // private void AnimationUpdate(){
+    //     state currState;
+    //     currState = state.run;
+    //     animate.SetInteger("currState", (int)currState);
+    // }
 }
