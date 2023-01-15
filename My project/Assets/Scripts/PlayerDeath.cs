c6f3acf8f6309440e9b35bdd22240672bfc409f5
DerpyMissile <hmai015@ucr.edu>
2023-01-15 00:11:44 -0800

    oh my god he so sexy

--- My project/Assets/Scripts/PlayerDeath.cs
+++ My project/Assets/Scripts/PlayerDeath.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerDeath : MonoBehaviour
 {
-    int howManyDeaths = 0;
-    int lives = 2;
     public Camera theCam;
     public GameObject blackScreenOfCertainDeath;
     public GameObject literallyEverythingElse;
-    public TextMeshPro deadText;
+    public TextMeshProUGUI deadText;
+    public GameObject[] hearts;
+    public GameObject stage2heart1;
+    public GameObject stage2heart2;
+    public GameObject stage3heart1;
+    public GameObject stage3heart2;
+    public GameObject stage2;
+    public GameObject stage3;
 
     void Start(){
         blackScreenOfCertainDeath.GetComponent<SpriteRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         blackScreenOfCertainDeath.SetActive(false);
         literallyEverythingElse.GetComponent<SpriteRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        stage2heart1.SetActive(false);
+        stage2heart2.SetActive(false);
+        stage3heart1.SetActive(false);
+        stage3heart2.SetActive(false);
+        hearts = GameObject.FindGameObjectsWithTag("Heart");
     }
 
     IEnumerator firstDeath(){
@@ -38,36 +49,59 @@ public class PlayerDeath : MonoBehaviour
         yield return new WaitForSeconds(1);
         blackScreenOfCertainDeath.GetComponent<SpriteRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.7f);
         literallyEverythingElse.GetComponent<SpriteRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.2f);
-        deadText.SetText = "HAHA! LOOKS LIKE YE LOSE!";
+        deadText.SetText("HAHA! LOOKS LIKE YE LOSE!");
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
-        deadText.SetText = "What? Rigged? Barely playable? Unfair?? What are you talking about; the game was perfectly balanced.";
+        yield return new WaitForSeconds(1);
+        deadText.SetText("What? Rigged? Barely playable? Unfair?? What are you talking about; the game was perfectly balanced.");
+        yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
-        deadText.SetText = "DESIGNED TO SUCK QUARTERS OUT OF CHILDREN??";
+        deadText.SetText("DESIGNED TO SUCK QUARTERS OUT OF CHILDREN??");
+        yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
-        deadText.SetText = "So what if it is?! We have to make money somehow-";
+        deadText.SetText("So what if it is?! We have to make money somehow-");
+        yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
-        deadText.SetText = "You're going to sue us for using Dwayne \"The Rock\" Johnson's face without his consent for our rigged game...";
+        deadText.SetText("You're going to sue us for using Dwayne \"The Rock\" Johnson's face without his consent for our rigged game...");
+        yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
-        deadText.SetText = "ALRIGHT FINE!... You get 1 more try on the house.";
+        deadText.SetText(".............");
+        yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
-        deadText.SetText = "And here; since I'm feeling generous and benevolent... HUZZAH! 2 MORE LIVES!";
+        deadText.SetText("ALRIGHT FINE!... You get 1 more try on the house.");
+        yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
-        deadText.SetText = "...INSERTED THE COIN YET? OK GOOD! GREAT, EVEN! OI'VE MODIFIED THE GAME SO EVEN *PANSIES* LIKE YOU CAN COMPLETE IT! NOW SCRAM!";
-        lives = 4;
+        deadText.SetText("And here; since I'm feeling generous and benevolent... HUZZAH! 2 MORE LIVES!");
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        deadText.SetText("...INSERTED THE COIN YET? OK GOOD! GREAT, EVEN! OI'VE MODIFIED THE GAME SO EVEN *PANSIES* LIKE YOU CAN COMPLETE IT! NOW SCRAM!");
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        PlayerStats.lives = 4;
+        while(theCam.orthographicSize > 5){
+            theCam.orthographicSize-=4;
+            yield return new WaitForSeconds(1);
+        }
         StopCoroutine(firstDeath());
+        prepareSecondStage();
+    }
+
+    void prepareSecondStage(){
+        stage2heart1.SetActive(true);
+        stage2heart2.SetActive(true);
     }
 
     void OnCollisionEnter2D(Collision2D collision){
         
         if(collision.gameObject.tag != "Platform"){
             Debug.Log("ded");
-            if(lives <= 0){
-                if(howManyDeaths == 0){
+            hearts[PlayerStats.lives].AddComponent<Rigidbody2D>();
+            if(PlayerStats.lives <= 0){
+                if(PlayerStats.howManyDeaths == 0){
                     StartCoroutine(firstDeath());
-                    howManyDeaths++;
+                    PlayerStats.howManyDeaths++;
                 }
             }else{
-                lives -= 1;
+                PlayerStats.lives -= 1;
             }
         }
     }
---
+++ My project/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerStats{
+    public static int lives = 2;
+    public static int howManyDeaths = 0;
+    public static bool nextLevel = true;
+}
