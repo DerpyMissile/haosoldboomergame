ba46528a3e74e41e295d3a23e667ba62406ca387
Michael-Pham1 <47127433+Michael-Pham1@users.noreply.github.com>
2023-01-14 16:40:26 -0800

    Prevents asteroid collision with other enemies

--- My project/Assets/Scripts/Asteroid.cs
+++ My project/Assets/Scripts/Asteroid.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision){
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = newSprite; 
+    }
+
+    IEnumerator hi(){
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag != "Platform" && collision.gameObject.tag != "Player"){
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), this.GetComponent<Collider2D>());
+        }
         if(collision.gameObject.tag == "Platform"){
-            Destroy(gameObject);
+            ChangeSprite();
+            StartCoroutine(hi());
         }
     }
 }
--- My project/Assets/Scripts/PlayerDeath.cs
+++ My project/Assets/Scripts/PlayerDeath.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerDeath : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision){
+        
         if(collision.gameObject.tag != "Platform"){
             Application.Quit();
             Debug.Log("ded");
